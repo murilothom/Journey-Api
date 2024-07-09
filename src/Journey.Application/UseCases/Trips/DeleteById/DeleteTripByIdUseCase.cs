cd2af01b086eb68f45fa7bfd90da7d2204e39910
Murilo Thom <murilothom.2002@hotmail.com>
2024-07-09 15:27:27 -0300

    Remove unsed using

--- src/Journey.Application/UseCases/Trips/DeleteById/DeleteTripByIdUseCase.cs
+++ src/Journey.Application/UseCases/Trips/DeleteById/DeleteTripByIdUseCase.cs
@@ -1,11 +1,6 @@
-﻿using Journey.Exception.ExceptionsBase;
-using Journey.Exception;
+﻿using Journey.Exception;
+using Journey.Exception.ExceptionsBase;
 using Journey.Infrastructure;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace Journey.Application.UseCases.Trips.DeleteById
